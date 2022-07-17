b64c59952b4c013426af4f317d1fe597b502b516
André Vitorino <andre.vitorino@capgemini.com>
2022-07-17 15:24:15 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import NotificationButton from "./components/NotificationButton"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from "./components/Header"
 import SalesCard from "./components/SalesCard"
 
 function App() {
   return(
     <>
-      <Header/>
+      <ToastContainer/>
       <main>
       <section id="sales">
         <div className="dsmeta-container">
